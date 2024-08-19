63e1ef9af62ae8c8bffaf34ab192f7e46d7661ba
YonV <32718226+YonVclaw@users.noreply.github.com>
2024-08-19 06:43:02 -0600

    next base

--- addons/loading/CfgLoadingScreen.hpp
+++ addons/loading/CfgLoadingScreen.hpp
@@ -9,30 +9,17 @@
 
 class GVAR(CfgLoadingScreen) {
     class Backgrounds {
-        LOADING_SCREEN_CLASS(269676636,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(269676677,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(271859839,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(285905473,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(296841733,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(297308106,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(298480575,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(361616315,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(287796119,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(288887578,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(319048089,61st Mechanized Infantry Battalion);
+        LOADING_SCREEN_CLASS(clearingoutHD,61st Mechanized Infantry Battalion);
+        LOADING_SCREEN_CLASS(debriefHD,61st Mechanized Infantry Battalion);
+        LOADING_SCREEN_CLASS(firefightHD,61st Mechanized Infantry Battalion);
+        LOADING_SCREEN_CLASS(HALOHD,61st Mechanized Infantry Battalion);
+        LOADING_SCREEN_CLASS(IgotyouHD,61st Mechanized Infantry Battalion);
+        LOADING_SCREEN_CLASS(mainroad,61st Mechanized Infantry Battalion);
+        LOADING_SCREEN_CLASS(nightflightHD,61st Mechanized Infantry Battalion);
+        LOADING_SCREEN_CLASS(planepose,61st Mechanized Infantry Battalion);
+        LOADING_SCREEN_CLASS(recoveryHD,61st Mechanized Infantry Battalion);
+        LOADING_SCREEN_CLASS(stackupHD,61st Mechanized Infantry Battalion);
+        LOADING_SCREEN_CLASS(WardaddyHD,61st Mechanized Infantry Battalion);
         LOADING_SCREEN_CLASS(maxresdefault,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(269677107,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(272813407,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(275306051,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(283924597,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(371890417,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(400370807,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(254754921,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(273347491,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(275458856,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(286142395,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(298331323,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(356376886,61st Mechanized Infantry Battalion);
-        LOADING_SCREEN_CLASS(S291207115895,61st Mechanized Infantry Battalion);
     };
 };
---
+++ addons/sig/config.cpp
@@ -0,0 +1,28 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {
+            QGVAR(XM7),
+            QGVAR(XM7_G),
+            QGVAR(XM7_BLK),
+            QGVAR(XM7_BLK_G)
+        };
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "lxim_main",
+            "KAR_XM7"
+        };
+        authorUrl = URL;
+        author = QAUTHOR;
+        authors[] = {""};
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEventHandlers.hpp"
+#include "CfgVehicles.hpp"
+#include "CfgWeapons.hpp"
+#include "xtdsig.hpp"
